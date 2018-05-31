7dc62a24d7a84df236c1bb3447d5cd4f6ad5f033
Sergey Koposov <skoposov@cmu.edu>
2018-05-31 04:03:29 -0400

    fix the description of the project on pypi bump version

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ def get_revision():
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
-VERSION = '0.0.3'
+VERSION = '0.0.4'
 #VERSION = VERSIONPIP+'dev'+get_revision()
 
 setup(
@@ -46,6 +46,7 @@ setup(
     package_dir={'':'py/'},
     package_data={'sqlutilpy':['tests/']},
     long_description=read('README.md'),
+    long_description_content_type='text/markdown',
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Topic :: Utilities",
