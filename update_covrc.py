efee0676a76631609cb36271e3bce0b8bee59f70
Sergey E. Koposov <skoposov@ed.ac.uk>
2021-05-03 14:08:05 +0100

    try to fix the .coveragerc generation

--- update_covrc.py
+++ update_covrc.py
@@ -1,12 +1,16 @@
 import sys
-def getmod_path():
-    import sqlutilpy
-    path = '/'.join(sqlutilpy.__file__.split('/')[:-1]) + '/'
+import importlib
+
+
+def getmod_path(modname):
+    mod = importlib.import_module(modname)
+    path = '/'.join(mod.__path__[0].split('/')[:-1]) + '/'
+    # path = '/'.join(path.split('/')[:-1]) + '/'
     return path
 
+
 with open('.coveragerc', 'w') as fd:
-    path = getmod_path()
-    print ('''[run]
-    source='''+path,file=fd)
-
-
+    path = getmod_path('sqlutilpy')
+    print(f'''[run]
+source={path}
+''', file=fd)
